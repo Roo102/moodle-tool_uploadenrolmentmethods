deecdb5e520e6e6da832aeb9623e3ddf2c88f511
Roo102 <87289477+Roo102@users.noreply.github.com>
2023-02-01 09:56:52 +1300

    Update version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_uploadenrolmentmethods';
-$plugin->version   = 2023013105;        // The current module version.
+$plugin->version   = 2023013106;        // The current module version.
 $plugin->requires  = 2018051700;        // Requires Moodle version 3.5 or higher.
-$plugin->release = '1.3.5 (Build: 2023013105)'; // Human readable version information.
+$plugin->release = '1.3.5 (Build: 2023013106)'; // Human readable version information.
 $plugin->maturity  = MATURITY_STABLE;

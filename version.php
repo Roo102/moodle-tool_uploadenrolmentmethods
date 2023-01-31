7ea07f08f37d2de20cb4c8184576056e4edc0c57
Roo102 <87289477+Roo102@users.noreply.github.com>
2023-01-31 15:09:55 +1300

    Append version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tool_uploadenrolmentmethods';
-$plugin->version   = 2023013102;        // The current module version.
+$plugin->version   = 2023013103;        // The current module version.
 $plugin->requires  = 2018051700;        // Requires Moodle version 3.5 or higher.
-$plugin->release = '1.3.5 (Build: 2023013102)'; // Human readable version information.
+$plugin->release = '1.3.5 (Build: 2023013103)'; // Human readable version information.
 $plugin->maturity  = MATURITY_STABLE;
